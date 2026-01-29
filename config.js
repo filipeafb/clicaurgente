60342cb9f90e3ca37d02bae881ba08ace0771bdd
filipeafb <filipeafb07@gmail.com>
2026-01-29 17:20:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,11 @@
 // ============================================
 
 const CONFIG = {
-    // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
-    valentineName: RITA,
+ valentineName: RITA
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: BABY URGENTE, PRECISO DA TUA AJUDA,
+    pageTitle: BABY URGENTE PRECISO DA TUA AJUDA
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
